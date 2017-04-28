8aaea9615f5c559f2ce7cba9f882e15363468be1
sam <sam@firstbuild.com>
2017-04-28 01:46:13 +0000

    Add water temp

--- index.js
+++ index.js
@@ -1,164 +1,221 @@
-var player = require('play-sound')()
-var greenBean = require('green-bean')
-var uname = require('node-uname')
-var fs = require('fs')
-var gotPi = false
+var gea = require('gea-sdk')
+var adapter = require('gea-adapter-usb')
+var say = require('say')
+var player = require('play-sound')({})
+var gpio = require('rpi-gpio')
 
-var unameInfo = uname()
-if (unameInfo.machine) {
-  if (unameInfo.machine.indexOf('x86') === 0) {
-  } else {
-    gotPi = true
-  }
-} else {
-  console.log('No machine info found.')
-}
-
-if (gotPi) {
-  var gpio = require('rpi-gpio')
-
-  gpio.on('change', function (channel, value) {
-    console.log(languages[language])
-    // When the button is released
-    if (value === false) {
-      language++
-      if (language > languages.length - 1) {
-        language = 0
-      }
-    }
-  })
-  gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH)
-}
-
-// All supported languages
-var languages = [
-  'en',
-  'es'
-]
-// Current language
-var language = 0
-
-// List of chimes
-var chimes = [
-  'chime',
-  'chime_big_ben',
-  'chime_x'
-]
-// Current chime
-var chime = 0
-
-var codes = {
-  1: 'basket_clean',
-  2: 'rinse_and_spin',
-  3: 'quick_rinse',
-  4: 'bulky_items',
-  5: 'sanitize',
-  6: 'towels_and_sheets',
-  7: 'steam_refresh',
-  8: 'normal',
-  9: 'whites',
-  10: 'darks',
-  11: 'jeans',
-  12: 'hand_wash',
-  13: 'delicates',
-  14: 'speed_wash',
-  15: 'heavy_duty',
-  16: 'allergen',
-  17: 'power_clean',
-  18: 'rinse_and_spin',
-  19: 'single_item',
-  20: 'colors',
-  21: 'cold_wash',
-  128: 'cottons',
-  129: 'easy_care',
-  130: 'active_wear',
-  131: 'time_dry',
-  132: 'dewrinkle',
-  133: 'air_fluff',
-  134: 'steam_refresh',
-  135: 'steam_dewrinkle',
-  136: 'speed_dry',
-  137: 'mixed',
-  138: 'speed_dry',
-  139: 'casuals',
-  140: 'warm_up',
-  141: 'energy-saver'
-}
-
-const INTERVAL = 60000
-const CHECK_INTERVAL = 15000
-const INTERVAL_COUNTER = INTERVAL / CHECK_INTERVAL
-
-greenBean.connect('laundry', function (laundry) {
-  console.log('Connected to some laundries')
-
-  var requestCycleSelectedStatus = (function (callback) {
-    var oldSelection = -10
-    return function () {
-      laundry.cycleSelected.read(function (value) {
-        // Value parameter is the code returned by the washer describing the cycle selected
-        if (value === 0 || codes[value] === undefined) {
-          console.error('Selection is Undefined or unknown: ' + value)
-        } else {
-          console.log('New selection : ' + value + ', which is ' + codes[value])
-          if (value === oldSelection) {
-            console.log('The selection did not change.')
-            return
-          }
-          oldSelection = value
-          console.log('Playing a sound for the new selection.')
-          playSound(getCycleCodeSoundPath(value))
-        }
-      })
-    }
-  })()
-
-  var requestMachineStatus = (function () {
-    var beepCount = 0
-    var isEndOfCycle = false
-    return function (laundry, intervalCounter) {
-      console.log('beepCount: ' + beepCount, ' isEndOfCycle: ' + isEndOfCycle, +' ' + intervalCounter)
-      laundry.machineStatus.read(function (machineStatus) {
-        // 4: End of cycle
-        if (machineStatus === 4) {
-          isEndOfCycle = true
-        } else {
-          // 0: Off (We still want isEndOfCycle to be true when it is off)
-          if (machineStatus !== 0) {
-            beepCount = 0
-            isEndOfCycle = false
-          }
-        }
-        if (isEndOfCycle) {
-          beepCount++
-          if (beepCount % intervalCounter === 0) {
-            beepCount = 0
-            console.log('playing buzzer')
-            playSound('./buzzers/' + chimes[chime] + '.wav')
-          }
-        }
-      })
-    }
-  })()
-
-  setInterval(function () {
-    console.log('Requesting cycle status.')
-    requestCycleSelectedStatus()
-  }, 1000)
-
-  setInterval(function () {
-    requestMachineStatus(laundry, INTERVAL_COUNTER)
-  }, CHECK_INTERVAL)
+var app = gea.configure({
+  address: 0xcb,
+  version: [ 0, 0, 1, 0 ]
 })
 
-function getCycleCodeSoundPath (code) {
-  return './voices/' + languages[language] + '/' + codes[code] + '.mp3'
+gpio.on('change', function (channel, value) {
+  states[0].sayTime = true
+  console.log('button')
+})
+gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH)
+
+var SOURCE = 0xcb
+
+var WASHER = 0x23
+var DRYER = 0x2b
+
+var TIME_SECS = 0x2007
+var TIME_MINS = 0x0046
+var CYCLE_SELECTED = 0x200A
+var END_CYCLE = 0x2002
+var WATER_TEMP = 0x2016
+var MACHINE_STATUS = 0x2000
+
+var states = [
+  {
+    id: WASHER,
+    oldCycle: 0,
+    cycleRunAlert: false,
+    oldMinutesRemaining: 0,
+    name: 'washer'
+  },
+  {
+    id: DRYER,
+    oldCycle: 0,
+    cycleRunAlert: false,
+    oldMinutesRemaining: 0,
+    name: 'dryer'
+  }
+]
+
+app.bind(adapter, function (bus) {
+  bus.on('publish', function (erd) {
+    switch (erd.erd) {
+      case MACHINE_STATUS:
+        var machineStatus = erd.data[0]
+        for (var state in states) {
+          if (erd.source === states[state].id) {
+            if (machineStatus === 2) {
+              if (states[state].cycleRunAlert) {
+                states[state].cycleRunAlert = false
+                say.speak(
+                    'Starting '
+                    .concat(getReadableCycleName(states[state].oldCycle))
+                    .concat(', with an estimated ')
+                    .concat(states[state].oldMinutesRemaining)
+                    .concat(' minutes remaining.')
+                )
+              }
+            } else {
+              states[state].cycleRunAlert = true
+            }
+          }
+        }
+        break
+
+      case WATER_TEMP:
+        var tempCode = erd.data[0]
+        console.log(tempCode)
+        say.speak('Water temperature, '.concat(getTempByCode(tempCode)))
+        break
+
+      case TIME_SECS:
+        var bytes = erd.data
+        for (var state in states) {
+          if (erd.source === states[state].id) {
+            var minutes = Math.round((bytes[0]*255 + bytes[1])/60)
+            states[state].oldMinutesRemaining = minutes
+          }
+        }
+        break
+
+      case TIME_MINS:
+        var timeRemaining = erd.data[1]
+        for (var state in states) {
+          if (erd.source === states[state].id) {
+            states[state].oldMinutesRemaining = timeRemaining
+            if (states[state].sayTime) {
+              say.speak(timeRemaining + ' minutes left', 0.1)
+              states[state].sayTime = false
+            }
+          }
+        }
+        break
+
+      case CYCLE_SELECTED:
+        var cycleSelected = erd.data[0]
+        for (var state in states) {
+          if (erd.source === states[state].id) {
+            if (cycleSelected !== states[state].oldCycle) {
+                  // console.log('new', states[state].name, 'cycle:', cycleSelected)
+              say.speak(getReadableCycleName(cycleSelected))
+            } else {
+                  // console.log('same cycle')
+            }
+            states[state].oldCycle = cycleSelected
+          }
+        }
+        break
+
+      case END_CYCLE:
+        var cycleEnd = erd.data[0]
+        for (var state in states) {
+          if (erd.source === states[state].id) {
+              // states[state].
+          }
+        }
+        break
+    }
+  })
+  busSubscribe(bus, SOURCE, TIME_SECS, [DRYER])
+  busSubscribe(bus, SOURCE, TIME_MINS, [WASHER])
+  busSubscribe(bus, SOURCE, CYCLE_SELECTED, [WASHER, DRYER])
+  busSubscribe(bus, SOURCE, WATER_TEMP, [WASHER, DRYER])
+  busSubscribe(bus, SOURCE, MACHINE_STATUS, [WASHER, DRYER])
+})
+
+function busSubscribe (bus, source, erd, destinations) {
+  destinations.map(function (dest) {
+    bus.subscribe({
+      erd: erd,
+      source: source,
+      destination: dest
+    })
+  })
 }
 
-function playSound (path) {
-  if (fs.existsSync(path)) {
-    player.play(path)
-  } else {
-    console.error('Sound file does not exist: ' + path)
-  }
+var busRead = function (bus, source, erd, destinations) {
+  destinations.map(function (dest) {
+    bus.read({
+      erd: erd,
+      source: source,
+      destination: dest
+    })
+  })
 }
+
+var getTempByCode = (function () {
+  var codes = {
+    21: 'hot',
+    20: 'warm',
+    19: 'colors',
+    18: 'cool',
+    17: 'cold',
+    16: 'tap cold'
+  }
+
+  return function (code) {
+    return codes[code]
+  }
+})()
+
+var getCycleByCode = (function () {
+  var codes = {
+    1: 'basket_clean',
+    2: 'rinse_and_spin',
+    3: 'quick_rinse',
+    4: 'bulky_items',
+    5: 'sanitize',
+    6: 'towels_and_sheets',
+    7: 'steam_refresh',
+    8: 'normal',
+    9: 'whites',
+    10: 'darks',
+    11: 'jeans',
+    12: 'hand_wash',
+    13: 'delicates',
+    14: 'speed_wash',
+    15: 'heavy_duty',
+    16: 'allergen',
+    17: 'power_clean',
+    18: 'rinse_and_spin',
+    19: 'single_item',
+    20: 'colors',
+    21: 'cold_wash',
+    128: 'cottons',
+    129: 'easy_care',
+    130: 'active_wear',
+    131: 'time_dry',
+    132: 'dewrinkle',
+    133: 'air_fluff',
+    134: 'steam_refresh',
+    135: 'steam_dewrinkle',
+    136: 'speed_dry',
+    137: 'mixed',
+    138: 'speed_dry',
+    139: 'casuals',
+    140: 'warm_up',
+    141: 'energy_saver'
+  }
+
+  return function (code) {
+    return codes[code]
+  }
+})()
+
+var getReadableCycleName = function (code) {
+  return getCycleByCode(code)
+         .replace(/[_-]/g, ' ')
+}
+
+var playCycle = (function () {
+  return function (code) {
+    player.play('./voices/en/' + getCycleByCode(code) + '.mp3')
+  }
+})()
